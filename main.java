3b73139eccc01b1907533ebd1d9414dad0b21d63
Marc <marc63570@gmail.com>
2021-05-10 14:10:12 -0400

    Added frequency variable and method to populate it

--- main.java
+++ main.java
@@ -3,12 +3,20 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        // Initalise Variables
+        int[] frequency = new int[9]; // 9 indexes for each number (1, 2, 3, 4, etc)
         // Get the file
         File file = getFile();
         Scanner reader = new Scanner(file);
 
+        // Get the frequency of each number in the file
+        frequency = populateFrequency(file);
+
         // Close the scanner
         reader.close();
+    }
+    public static int[] populateFrequency(File file) {
+
     }
     public static File getFile() {
         Scanner reader = new Scanner(System.in);
